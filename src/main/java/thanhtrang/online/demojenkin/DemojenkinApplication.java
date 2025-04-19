9edd17a31e5cbd69c23952a39001472e612bfd34
thanhcom <anhchang00@gmail.com>
2025-04-19 12:55:00 +0700

    first commit

---
+++ src/main/java/thanhtrang/online/demojenkin/DemojenkinApplication.java
@@ -0,0 +1,13 @@
+package thanhtrang.online.demojenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojenkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thanhtrang/online/demojenkin/DemojenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package thanhtrang.online.demojenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojenkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
